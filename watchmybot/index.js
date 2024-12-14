368648defed5c7e2e7710ee2cc2d39daef3a4dbc
Julian Einzinger <julian5einzinger@gmail.com>
2024-12-14 18:42:43 +0100

    HTTP

--- watchmybot/index.js
+++ watchmybot/index.js
@@ -13,7 +13,7 @@ document.getElementById('newsletter-form').addEventListener('submit', function(e
     };
 
     // POST-Anfrage an den Server senden
-    fetch('https://185.253.17.65:8443', {
+    fetch('http://185.253.17.65:8443', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json' // Datenformat JSON
